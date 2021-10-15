8e01e2ce9dfb6558c5333d01e2d1a7d12b1e7a6b
Isabelle Lafont <isalafont@gmail.com>
2021-10-15 14:39:52 +0200

    Update rails version and Ruby version Fix webpack
    localhost is working now.

--- config/application.rb
+++ config/application.rb
@@ -1,19 +1,6 @@
 require_relative 'boot'
 
-require "rails"
-# Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
-require "active_record/railtie"
-require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_mailbox/engine"
-require "action_text/engine"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -21,11 +8,6 @@ Bundler.require(*Rails.groups)
 
 module Portfolio
   class Application < Rails::Application
-    config.generators do |generate|
-      generate.assets false
-      generate.helper false
-      generate.test_framework :test_unit, fixture: false
-    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
@@ -33,12 +15,5 @@ module Portfolio
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-
-    # Don't generate system test files.
-    config.generators.system_tests = nil
-
-    # Setting personnal fonts
-    # config.assets.paths << Rails.root.join('app', 'assets', 'fonts')
-    # config.assets.precompile << /\.(?:svg|eot|woff|ttf)$/
   end
 end
